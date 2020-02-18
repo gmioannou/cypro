8ac99950f4cb0ca186a94a00c85c936d2cbddc34
gmioannou <gmioannou@gmail.com>
2020-02-18 18:29:41 +0200

    update version 0.6 for new release

--- src/app/popover/popover.component.ts
+++ src/app/popover/popover.component.ts
@@ -34,7 +34,7 @@ export class PopoverComponent implements OnInit {
     this.presentAlert(`
       <p>The CyCLOPS Project</p>             
       <p>Crowdsourcing App for collecting data related to <strong>Natural Hazard</strong> events.</p>
-      <p>&copy 2020, Cyprus University of Technology</p> Version 0.5`)
+      <p>&copy 2020, Cyprus University of Technology</p> Version 0.6`)
     this.popoverController.dismiss();
 
   }
